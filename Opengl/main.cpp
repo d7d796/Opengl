2bb688972c08b5b6cf59004574fba68552534ece
انا <abdurhman@macbook-pro-alkhas-b-abdurhman-2.local>
2020-05-27 04:25:27 +0300

    Initial Commit

---
+++ Opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Opengl
+//
+//  Created by abdurhman on 04/10/1441 AH.
+//  Copyright © 1441 abdurhman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
